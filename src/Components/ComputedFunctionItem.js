250c02f6ae761a6d1e80a29a622c0098ef752af8
DESKTOP-4CDNPFM\Egor <luk_96@mail.ru>
2021-04-07 22:38:51 +0300

    правки в связи с изменениями в функции getLast на бэке

--- src/Components/ComputedFunctionItem.js
+++ src/Components/ComputedFunctionItem.js
@@ -5,45 +5,30 @@ function ComputedFunctionItem(props) {
 				style={{fontSize:"xx-large", width:"100%", marginBottom:"10px"}}>
 			<tr>
 				<td	colSpan="2">
-				{"F(" + props.parameters.map(p => p.name).join(",") + ") = " + props.expressionString}
+				{props.functionNotation}
 				</td>
 			</tr>
 			<tr>
 				<td>
 				Параметры:
 				</td>
-				
 				<td>
 				Значение:
 				</td>
 			</tr>
 			{
-				props.points.map(point => 
+				props.parametersAndValues.map(pv => 
 				{
-					return (<tr>
-							<td> {"F(" + createParamString(props.parameters, point) +")"} </td>
-							<td> {point.result} </td>
+					return (
+						<tr>
+							<td> {pv.parameters} </td>
+							<td> {pv.value} </td>
 						</tr>)
 				})
 			}
 			
 		</table>
 	);
-}
-
-function createParamString(parameters, point){
-	let paramStr = "";
-	if(parameters.length != 0)
-	{
-		paramStr = parameters.map(p => p.values.filter(v => v.pointId == point.id).map(v => 
-			{
-				const result = v.value.toString();
-				return result;
-			})[0])
-			.join(",");
-	}
-	
-	return paramStr;
 }
 	
 export default ComputedFunctionItem;
